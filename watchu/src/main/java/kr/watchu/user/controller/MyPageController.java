34098ac202f97d23741dd02e7364984b29406381
user1 <user1@user1-PC>
2018-08-28 14:37:40 +0900

    예림2

--- watchu/src/main/java/kr/watchu/user/controller/MyPageController.java
+++ watchu/src/main/java/kr/watchu/user/controller/MyPageController.java
@@ -28,7 +28,7 @@ public class MyPageController {
 	}
 	
 	//마이페이지 메인
-	@RequestMapping("/user/mypage.do")
+	@RequestMapping("/user/userMypage.do")
 	public String mypage(HttpSession session,Model model) {
 		String id = (String)session.getAttribute("user_id");
 		UserCommand user = userService.selectUser(id);
@@ -39,31 +39,31 @@ public class MyPageController {
 		
 		model.addAttribute("user", user);
 		
-		return "mypage";
+		return "userMypage";
 	}
 	
 	//평가한 영화 목록
-	@RequestMapping("/user/mypage_movie.do")
+	@RequestMapping("/user/userMypage_movie.do")
 	public String mypage_movie() {
-		return "mypage_movie";
+		return "userMypage_movie";
 	}
 	
 	//평가한 영화 목록 더보기
 	@RequestMapping("/user/mypage_movielist.do")
 	public String mypage_movielist() {
-		return "mypage_movielist";
+		return "userMypage_movielist";
 	}
 	
 	//코멘트
 	@RequestMapping("/user/comment.do")
 	public String comment() {
-		return "comment";
+		return "userComment";
 	}
 	
 	//코멘트 상세페이지
 	@RequestMapping("/user/comment_detail.do")
 	public String comment_detail() {
-		return "comment_detail";
+		return "userComment_detail";
 	}
 		
 }
